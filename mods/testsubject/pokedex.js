fd37a180ce1c801c5f8b6f162988b9326890acad
Oesher Ayenew <OIawesome98@gmail.com>
2014-04-08 11:42:15 -0700

    Update pokedex.js

--- mods/testsubject/pokedex.js
+++ mods/testsubject/pokedex.js
@@ -1,2 +1,3 @@
+exports.BattlePokedex = {
 gengarmega:{num:94,species:"Gengar-Mega",baseSpecies:"Gengar",forme:"Mega",formeLetter:"M",types:["Ghost","Poison"],baseStats:{hp:60,atk:65,def:80,spa:170,spd:95,spe:130},abilities:{0:"Shadow Tag"},heightm:1.4,weightkg:40.5,color:"Purple",prevo:"haunter",evoLevel:25,eggGroups:["Amorphous"]}
-;
+};
